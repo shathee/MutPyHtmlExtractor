91155d1018515c104964ab94a48dbaf7673a021d
Baki <sat.sust@gmail.com>
2021-02-26 03:08:40 +0100

    function calls in main body done. working properly. comments added

--- html-parse.py
+++ html-parse.py
@@ -3,9 +3,16 @@ import os, glob, sys, re, fileinput, argparse
 from bs4 import BeautifulSoup
 import csv
 
+
+'''
+finds all html files in the folder and subfolders 
+'''
 def collect_files_from_directory(pathToDirectory):
     return glob.glob(pathToDirectory +r"/**/*.html", recursive=True)
 
+'''
+parse the file given as parameter and saves the information in csv format
+'''
 def parse_index_html(pathToDirectory):
 	soup = BeautifulSoup (open(pathToDirectory), features="lxml")
 
@@ -39,3 +46,37 @@ def parse_index_html(pathToDirectory):
 	
 	f.writerow([no_of_tests[0],mutation_score[0],successful_mutants[0],survived_mutants[0],incompetent_mutants[0],timeout_mutants[0]])
 
+
+file_list = collect_files_from_directory('html_report')
+
+for f in file_list:
+	if(re.search(r"(index)(\.html)$",f)):
+		parse_index_html(f)
+	elif(re.search(r"([0-9])(\.html)$",f)):
+		print('2nd typ', f)
+	else:
+		print("no Html file found")
+
+
+# soup = BeautifulSoup(html_text, 'html.parser')
+# 	with open(f, 'r', encoding='utf-8') as infile:
+# 		for line in infile:
+# 			print(line)
+
+
+
+
+
+# mutatant_total = mutation_time_tag.parent.parent
+# print(mutatant_total)
+# \<\/(strong)\>(.)
+# print(mutation_score.find_previous_siblings("strong"))
+
+# for link in links:
+	# print(link)
+    # names = link.contents[0]
+    # fullLink = link.get('class')
+
+    # f.writerow([no_of_tests,mutation_score,successful_mutants,survived_mutants,incompetent_mutants,timeout_mutants])
+# for span in soup.find_all('span'):
+#     print(span.get('class'))
