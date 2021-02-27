be3cd09f111f6d3c100b298a5d7a6a2a9aa859b9
Baki <sat.sust@gmail.com>
2021-02-27 01:56:21 +0100

    gui added

--- test.py
+++ test.py
@@ -1,65 +1,115 @@
 from tkinter import *
-import os
+from tkinter.ttk import Frame, Button, Label, Style, Radiobutton
 from tkinter import filedialog
 
- 
-root = Tk()
-root.geometry("500x350")
-frame = Frame(root)
-frame.grid()
- 
+
+class MainGuiClass(Frame):
+
+    def __init__(self):
+        super().__init__()
+        self.grid(column = 4,row = 4,padx = 50,pady = 50)
+        self.select_source_type_var = IntVar()
+        self.source = StringVar()
+        self.initUI()
+
+    def type_sel(self):
+        selection = self.select_source_type_var
+        return selection.get()
+
+    def source_select(self):
+        type = self.type_sel()
+        if(type == 1):
+            filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+        elif(type == 2):
+            filename = filedialog.askdirectory(initialdir = "/")
+        else:
+            filename = 'no type selected'
+        
+        self.source.set(filename)
+        
+
+    def initUI(self):
+
+        self.master.title("MutPy Gui")
+        self.pack(fill=BOTH, expand=True)
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.columnconfigure(3, pad=5, weight=1)
+        self.rowconfigure(3, weight=1)
+        self.rowconfigure(5, pad=7)
 
 
-source_type = StringVar()
 
-def get_type():
-	return source_type.get()
-def select_source_folder():
-	global source_folder
-	folder_name = filedialog.askdirectory()
-	return source_folder.set(folder_name)
+        lbl = Label(self, text="Please select preferred options", font=("Arial", 25))
+        lbl.grid(row=0, column=0, columnspan=4, pady=10)
 
-label = Label(frame, text = "Please select Preferred Options ")
-label.grid(row = 0, column = 0, columnspan = 4)
+        
+        source_select_radio_type_lbl = Label(self, text="Please select Source type")
+        source_select_radio_type_lbl.grid(pady=5, padx=5, columnspan=2)
+        source_select_radio_type_btn1 = Radiobutton(self,text="File", variable=self.select_source_type_var, value=1, command=self.type_sel)
+        source_select_radio_type_btn1.grid(row=1, column=2, pady=5, padx=5)
+        source_select_radio_type_btn2 = Radiobutton(self,text="Folder/Directory", variable=self.select_source_type_var, value=2, command=self.type_sel)
+        source_select_radio_type_btn2.grid(row=1, column=3, pady=5, padx=5)
+        
+        source_selector_btn = Button(self, text="Select Source to Mutate", command=self.source_select)
+        source_selector_btn.grid(row=2, column=2)
 
-label = Label(frame, text = "Please select Source Type to mutate ")
-label.grid(row = 1, column = 0, columnspan = 2)
-Rbttn_file = Radiobutton(frame, text = "File", variable = source_type, value = 1, command=get_type)
-Rbttn_file.grid(row = 1, column = 2, padx = 3, pady = 3)
-Rbttn_directory = Radiobutton(frame, text = "Folder", variable = source_type, value = 2, command=get_type)
-Rbttn_directory.grid(row = 1, column = 3, padx = 3, pady = 3)
-label = Label(frame, text = get_type)
-label.grid(row = 2, column = 0)
+        source_lbl = Label(self, textvariable=self.source, font=("Arial", 12))
+        source_lbl.grid(row=3, column=0, columnspan=4, pady=5)
+
+        mutation_operator_dict = {
+            "AOD":"arithmetic operator deletion",
+            "AOR":"arithmetic operator replacement",
+            "ASR":"assignment operator replacement",
+            "BCR":"break continue replacement",
+            "COD":"conditional operator deletion",
+            "COI":"conditional operator insertion",
+            "CRP":"constant replacement",
+            "DDL":"decorator deletion",
+            "EHD":"exception handler deletion",
+            "EXS":"exception swallowing",
+            "IHD":"hiding variable deletion",
+            "IOD":"overriding method deletion",
+            "IOP":"overridden method calling position change",
+            "LCR":"logical connector replacement",
+            "LOD":"logical operator deletion",
+            "LOR":"logical operator replacement",
+            "ROR":"relational operator replacement",
+            "SCD":"super calling deletion",
+            "SCI":"super calling insert",
+            "SIR":"slice index remove",
+        }
+
+        operator_listbox = Listbox (self)
+        operator_listbox.grid(row=4, column=0, columnspan=4, pady=5)
+        for key in mutation_operator_dict:
+            operator_listbox.insert(END, key) 
+            
+
+            # lstbox.insert(END, '{}: {}'.format(key, d[key]))
+
+        # abtn = Button(self, text="Select Source to Mutate")
+        # abtn.grid(row=2, column=2)
+
+        # cbtn = Button(self, text="Close")
+        # cbtn.grid(row=2, column=3, pady=4)
+
+        # hbtn = Button(self, text="Help")
+        # hbtn.grid(row=5, column=0, padx=5)
+
+        # obtn = Button(self, text="OK")
+        # obtn.grid(row=5, column=3)
 
 
-label = Label(frame, text = "Please select Source ")
-label.grid(row = 2, column = 0)
+def main():
 
-source_folder = StringVar()
+    root = Tk()
+    root.geometry("500x400")
+    app = MainGuiClass()
+    root.mainloop()
 
 
-file_folder_select_button = Button(frame, text = "Select Source", command = select_source_folder)
-file_folder_select_button.grid(row = 2, column = 1)
-lbl1 = Label(master=frame, textvariable=source_folder)
-lbl1.grid(row = 2, column = 2, columnspan = 2)
-
-print(source_folder)
-
-operator_list = [
-'AOD',
-"AOR",
-"ASR",
-"BCR",
-"COD",
-"COI",
-"CRP",
-"DDL"
-    ]
-operator_listbox = Listbox(frame)
-
-for i in range(len(operator_list)):
-    operator_listbox.insert(i,operator_list[i])  
-operator_listbox.grid()
- 
-root.title("Wrapper for Mut.py")
-root.mainloop()
+if __name__ == '__main__':
+    main()
