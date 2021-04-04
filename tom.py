0cb475700fa66ee7136ee12715bb1812e14383fc
Baki <sat.sust@gmail.com>
2021-04-05 01:27:03 +0200

    clean PIT from cmd added

--- tom.py
+++ tom.py
@@ -3,6 +3,7 @@ from csv_reader import generate_tom_run_vs_tc_2
 from csv_reader import generate_tom_run_vs_tc
 from csv_reader import generate_tom_run_tc_op
 from csv_reader import collect_csv_files_from_directory
+from csv_reader import pit_csv_cleaner
 
 
 def main(argv):
@@ -13,7 +14,7 @@ def main(argv):
 	if len(argv) <=0:
 		print("please provide valude arguments")
 	try:
-		opts, args = getopt.getopt(argv,"hf:d:",["file=","dir="])
+		opts, args = getopt.getopt(argv,"hf:d:cp:",["file=","dir=","cleanPIT="])
 	except getopt.GetoptError:
 		print ('tom.py -f <file> -d <directory>')
 		sys.exit(2)
@@ -24,21 +25,27 @@ def main(argv):
 		elif opt in ("-f", "--file"):
 			inputfile = arg
 			directory = False
+			generate_tom_run_tc_op(inputfile)
 		elif opt in ("-d", "--dir"):
 			inputdir = arg
-			directory = True
+			# directory = True
+			files = collect_csv_files_from_directory(inputdir)
+			for f in files:
+				generate_tom_run_tc_op(f)
+		elif opt in ("-cp", "--cleanPIT"):
+			pit_csv_cleaner(arg)
 
 
-	if(directory):
-		files = collect_csv_files_from_directory(inputdir)
-		for f in files:
-			# generate_tom_run_vs_tc(f)
-			# generate_tom_run_vs_tc_2(f)
-			generate_tom_run_tc_op(f)
-	else:
-		# generate_tom_run_vs_tc(inputfile)	
-		# generate_tom_run_vs_tc_2(inputfile)
-		generate_tom_run_tc_op(inputfile)
+	# if(directory):
+	# 	files = collect_csv_files_from_directory(inputdir)
+	# 	for f in files:
+	# 		# generate_tom_run_vs_tc(f)
+	# 		# generate_tom_run_vs_tc_2(f)
+	# 		generate_tom_run_tc_op(f)
+	# else:
+	# 	# generate_tom_run_vs_tc(inputfile)	
+	# 	# generate_tom_run_vs_tc_2(inputfile)
+	# 	generate_tom_run_tc_op(inputfile)
 
 
 
