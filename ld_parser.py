b19e63b77ce32de05c9e56b10069c6151061cb0c
Baki <sat.sust@gmail.com>
2021-04-26 23:16:09 +0200

    ld files parsing complete

--- ld_parser.py
+++ ld_parser.py
@@ -2,7 +2,7 @@ import os, glob, sys, re, fileinput, argparse
 import csv
 import yaml
 
-from collections import OrderedDict
+from collections import Counter
 '''
 finds all html files in the folder and subfolders 
 '''
@@ -15,20 +15,38 @@ def collect_all_files_from_directory_by_ext(path_to_directory, ext):
 
 def get_all_test_case_names(path_to_directory):
 	files = collect_all_files_from_directory_by_ext(path_to_directory, '.java')
-	print(files)
+	all_tests = []
+	for f in files:
+		with open(f) as of:
+			for line in of:
+				if "@Test" in line:
+					nl = next(of, '').strip()
+					all_tests.append(re.search(r'test[a-zA-Z_0-9]+', nl).group(0))
+	return all_tests
 
 def parse_ld_file():
 	files = collect_all_files_from_directory_by_ext('LDFiles','.txt')
-	
+	all_tests = get_all_test_case_names('LDFiles\\test')
+	# print(files)
 	mutation_data_list = []
+	
 	for f in files:
-		mutation_entity = {}
-		directory = '\\'.join(f.split('\\')[0:-1])
-		mutation_entity['operator'] = get_mutant_operator(directory+'\\'+f.split('\\')[-1].split('.')[0] +'.java')
-		mutation_entity['mutation_node'] = get_mutantion_node(directory+'\\'+f.split('\\')[-1].split('.')[0] +'.java')
-		print(get_failed_test(f))
-		mutation_data_list.append(mutation_entity)
-	print(mutation_data_list)
+		for tc in all_tests:
+			mutation_entity = {}
+			directory = '\\'.join(f.split('\\')[0:-1])
+			mutation_entity['test_case'] = tc
+			mutation_entity['status'] = get_failed_status(f, tc)
+			mutation_entity['operator'] = get_mutant_operator(directory+'\\'+f.split('\\')[-1].split('.')[0] +'.java')
+			mutation_entity['mutation_node'] = get_mutantion_node(directory+'\\'+f.split('\\')[-1].split('.')[0] +'.java')
+			# print(mutation_entity)
+			mutation_data_list.append(mutation_entity)
+	# print(len(mutation_data_list))
+	write_to_csv(mutation_data_list)
+	c = Counter()
+	for item in mutation_data_list:
+	    c[item["status"]] += 1
+	print(c)
+
 
 
 def get_mutant_operator(file):
@@ -46,21 +64,24 @@ def get_mutantion_node(file):
 	return mn.strip()
 
 
-def get_tests(file):
-	with open(file) as f:
-		for line in f:
-			failed_test = re.search(r"Failed tests:", line) 
-			errored_test = re.search(r"Tests in error:", line) 
-			skipped_test = re.search(r"Tests in error:", line) 
-			 
-			if failed_test != None:
-				ft = failed_test.group(0)
-			if errored_test!=None: 
-				et = errored_test.group(0)
-	return ft
+def get_failed_status(file, tc):
+	textfile = open(file, 'r')
+	f = textfile.read()
+	r1 = re.search(r"(?s)(?<=Results :).*?(?=Tests run:)", f)
+	if r1 != None:
+		if tc in r1.group(0):
+			status = 'killed'
+		else:
+			status = 'survived'
+	else:
+		status = 'survived'
+	return status
 
-	
+def write_to_csv(list_of_dict):
+	for d in list_of_dict:
+		with open('data.csv', 'a+', newline='', encoding="Latin-1") as myfile:
+			wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+			wr.writerow([d['test_case'],d['operator'],d['mutation_node'],d['status']])
 
 
-# parse_ld_file()
-get_all_test_case_names()
+parse_ld_file()
